d589e1d3eb83f61a603d936087c01981cc488c87
Aubree Alexander <aubree.alexander@gmail.com>
2022-07-24 21:22:11 -0500

    indexeddb to cart

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,6 +6,7 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from '../../utils/helpers'
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -15,13 +16,30 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
+    //if there's data to be stored
     if (data) {
+      //store it in global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+
+      //take each product and save it to indexedDB using helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product)
+      })
+      //add else if to check if `loading` is undefined in `usequery()` hook
+    } else if (!loading) {
+      //since we're offline, get all of the data from the products store
+      idbPromise('products', 'get').then((products) => {
+        //use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        })
+      })
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
